39778ec9a6554aef299dc02891625bef64b9a73e
Manish Shaw <manishshaw323@gmail.com>
2019-04-10 13:57:13 +0530

    first commit

---
+++ my-first-maven-app/maven-demo/src/main/java/com/gontusoftware/demo/App.java
@@ -0,0 +1,13 @@
+package com.gontusoftware.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
