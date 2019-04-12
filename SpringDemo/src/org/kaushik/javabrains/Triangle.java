72ef4b735a7a983c3bfc53695da3029fa7177cd3
Manish Shaw <manishshaw323@gmail.com>
2019-04-12 13:48:33 +0530

    Added SpringDemo

---
+++ SpringDemo/src/org/kaushik/javabrains/Triangle.java
@@ -0,0 +1,14 @@
+package org.kaushik.javabrains;
+
+public class Triangle {
+	private String type;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void draw() {
+		System.out.print("Traingle drawn");
+	}
+}
